fa5aa00aedec8a39a38d9d39c2a8c2cc609af2f9
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-05-22 00:22:31 +0530

    saved model after training

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 # actual training
+import joblib
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from vectorizer import vectorize_dataset
@@ -13,6 +14,9 @@ def model_train(evaluation=False):
 
     # train
     model.fit(X_train_vectorized, y_train)
+
+    joblib.dump(model, "models/spam_detector_svm.pkl")
+    print("Model saved successfully to 'models/spam_detector_svm.pkl'!")
 
     # prediction
     predictions = model.predict(X_test_vectorized)
@@ -32,3 +36,30 @@ def model_train(evaluation=False):
 
 if __name__ == "__main__":
     model_train(evaluation=True)
+
+"""
+(26931, 20000)
+(6733, 20000)
+vectorizer is saved successfully!
+(26931, 20000)
+(6733, 20000)
+vectorizer is saved successfully!
+['spam' 'spam' 'ham' 'ham' 'spam' 'ham' 'ham' 'ham' 'spam' 'ham']
+Accuracy:  0.999405911183722
+
+--Classification Report--
+               precision    recall  f1-score   support
+
+         ham       1.00      1.00      1.00      3269
+        spam       1.00      1.00      1.00      3464
+
+    accuracy                           1.00      6733
+   macro avg       1.00      1.00      1.00      6733
+weighted avg       1.00      1.00      1.00      6733
+
+
+--Confusion Matrix--
+ [[3265    4]
+ [   0 3464]]
+
+"""
--- src/vectorizer.py
+++ src/vectorizer.py
@@ -27,3 +27,4 @@ def vectorize_dataset():
 
 
 vectorize_dataset()
+
