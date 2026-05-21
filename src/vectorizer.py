d5cf8df3ea847d88ad59841ec54ab4696f8e453e
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-05-21 23:13:42 +0530

    refactoring vectorizer

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,15 +1,29 @@
 # converting text-> vector
 from preprocess import load_and_preprocess
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
 import joblib
 
-X, y = load_and_preprocess()
 
-vectorizer = TfidfVectorizer(max_features=200000)
+def vectorize_dataset():
+    X, y = load_and_preprocess()
 
-X_vectorized = vectorizer.fit_transform(X)
-print(X_vectorized)
-print(X_vectorized.shape)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-joblib.dump(vectorizer, "models/tfidf_vectorizer.pkl")
-print("vectorizer is saved successfully!")
+    vectorizer = TfidfVectorizer(max_features=20000)
+
+    X_train_vectorized = vectorizer.fit_transform(X_train)
+    print(X_train_vectorized.shape)
+
+    X_test_vectorized = vectorizer.transform(X_test)
+    print(X_test_vectorized.shape)
+
+    joblib.dump(vectorizer, "models/tfidf_vectorizer.pkl")
+    print("vectorizer is saved successfully!")
+    return (X_train_vectorized, X_test_vectorized, y_train, y_test)
+
+
+vectorize_dataset()
